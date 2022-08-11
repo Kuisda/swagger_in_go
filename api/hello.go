6a7c81d4da095f3153adfcf5ca2ba9e05d0dbeba
Kuisda <yili4113@gmail.com>
2022-08-11 14:34:57 +0800

    simple practice for swagger in go

---
+++ api/hello.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// hello godoc
+// @Summary Say Hello
+// @Description Say Hello
+// @Tags api
+// @Acception json
+// @Produce json
+// @Router /test [get]
+
+//另apifox中的测试环境可以添加前置的端口号，如果使用/swagger的url访问则可以直接用localhost
+func Hello(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Hello"})
+
+}
+
+//swag init命令要在main.go所在的目录下执行，如果你的主函数是其他名字例如code.go,指令为swag init -g code.go
+//swagger会搜索从main.go所在目录下的所有包的所有函数是否有swagger形式的注释并形成文档
+//使用自定义的结构json返回的时候要在结构前加一个{object}或者object
+//
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"swagger_used/api"
+	"swagger_used/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	docs.SwaggerInfo.Title = "Swagger Example Api"
+	docs.SwaggerInfo.Description = "This is a sample server"
+	docs.SwaggerInfo.BasePath = ""
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+	r := gin.Default()
+	r.GET("/test", api.Hello) //golang规定
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
+}
